f8d0c5778233e3a7f55a84b338ff94c161c6f09c
Bryam Rodriguez <brodriguezayabr@gmail.com>
2017-06-06 09:32:07 -0500

    Add refresh on click

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-let requestStream = Rx.Observable.of('https://api.github.com/users');
+const refreshButton = jQuery('#refresh');
+
+let refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+let requestStream = refreshClickStream.startWith('startup click')
+  .map(function () {
+    const randomOffset = Math.floor(Math.random() * 500);
+    return 'https://api.github.com/users?since=' + randomOffset;
+  });
 
 let responseStream = requestStream.flatMap(function (requestUrl) {
   return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
 });
 
+const followSection = jQuery('#follow');
+
 responseStream.subscribe(function (response) {
+  followSection.empty();
   response.slice(-3).forEach(function (data) {
     const html = '<article>\
       <img src=' + data.avatar_url + ' />\
@@ -12,9 +23,13 @@ responseStream.subscribe(function (response) {
       <span>X</span>\
     </article>';
 
-    jQuery('#follow').append(html);
+    followSection.append(html);
   });
 });
+
+// let requestStream = Rx.Observable.merge(
+//   requestOnRefreshStream, startupRequestStream
+// );
 
 // requestStream.subscribe(function (requestUrl) {
 //   let responseStream = Rx.Observable.create(function (observer) {
